5127f4507ad71ff7e6ed4b4bf6551c705e01e355
Oksana <tsov.sana@gmail.com>
2020-12-15 15:12:30 +0200

    add lv

---
+++ MillMax/api_mail_send.php
@@ -0,0 +1,22 @@
+<?php
+if (isset($_POST)) {
+  $to = 'info@millmax.eu';
+  $subject = 'From millmax.eu';
+  $subject = '=?UTF-8?B?' . base64_encode($subject) . '?=' . PHP_EOL;
+  $message = '<table cellpadding=\'0\' cellspacing=\'0\' border=\'0\'>';
+  foreach($_POST as $key => $value) {
+    $message .= '<tr><td align=\'right\'>'.$key.':</td><td>&nbsp;<b>'.Trim(stripslashes($value)).'</b></td></tr>';
+  }
+  $message .= '</table>';
+  $headers = 'From: robot@millmax.eu' . PHP_EOL . 'Reply-To: robot@millmax.eu' . PHP_EOL . 'Content-Type: text/html; charset=UTF-8' . PHP_EOL . 'MIME-Version: 1.0' . PHP_EOL . 'Content-Transfer-Encoding: 8bit ' . PHP_EOL;
+
+  $mail = mail($to, $subject, $message, $headers);
+
+  if($mail) {
+    header('Location: /');
+   	die();
+  } else {
+    print_r(400);
+  }
+}
+?>
